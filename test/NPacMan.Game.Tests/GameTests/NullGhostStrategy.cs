3de1db445d4fd987e34c032b4fb4b12ac029dc29
David Betteridge <DavidaBetteridge@gmail.com>
2020-05-09 15:39:52 +0100

    Remove duplicate files

--- test/NPacMan.Game.Tests/GameTests/NullGhostStrategy.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NPacMan.Game.Tests.GameTests
-{
-    public class NullGhostStrategy : IGhostStrategy
-    {
-        public Direction? GetNextDirection(Ghost ghost, Game game)
-            => null;
-    }
-}
