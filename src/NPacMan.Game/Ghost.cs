5400d1ff01f777821f1bbb04939f4d16cfa68efe
David Betteridge <DavidaBetteridge@gmail.com>
2020-05-17 17:38:30 +0100

    Most of the time this allocation isn't needed

--- src/NPacMan.Game/Ghost.cs
+++ src/NPacMan.Game/Ghost.cs
@@ -36,18 +36,11 @@ namespace NPacMan.Game
 
         public Ghost Move(Game game)
         {
-            var newDirection = CurrentStrategy.GetNextDirection(this, game);
-
-            var newLocation = newDirection switch
-            {
-                Direction.Up => Location.Above,
-                Direction.Down => Location.Below,
-                Direction.Left => Location.Left,
-                Direction.Right => Location.Right,
-                _ => Location
-            };
-
-            return WithNewLocationAndDirection(newLocation, newDirection ?? Direction);
+            var nextDirection = CurrentStrategy.GetNextDirection(this, game);
+            if (nextDirection is Direction newDirection)
+                return WithNewLocationAndDirection(Location + newDirection, newDirection);
+            else
+                return this;
         }
 
         public Ghost Chase()
