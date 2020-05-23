5a312c6f73274dc41cabf48de6bea360a65fa0d9
Peter Gillett <peter@gillett.co.uk>
2020-05-23 09:48:10 +0100

    Added an internal background display buffer

---
+++ NPacMan.SharedUi/Display.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.XPath;
+
+namespace NPacMan.SharedUi
+{
+    public class Display
+    {
+        private SpriteSource[,] _screen;
+        private SpriteSource[,] _buffer;
+
+        private int _height;
+        private int _width;
+
+        public Display()
+        {
+            _screen = new SpriteSource[1,1];
+            _buffer=new SpriteSource[1,1];
+
+            _height = 1;
+            _width = 1;
+        }
+
+        public void Size(int width, int height)
+        {
+            if (width != _width || height != _height)
+            {
+                _height = height;
+                _width = width;
+
+                _screen = new SpriteSource[_width, _height];
+                _buffer = new SpriteSource[_width, _height];
+            }
+        }
+
+        private IEnumerable<SpriteDisplay> GetBackgroundToDraw()
+        {
+            var toDraw = new List<SpriteDisplay>();
+            for (int y = 0; y < _height; y++)
+            {
+                for (int x = 0; x < _width; x++)
+                {
+                    var s = _screen[x, y];
+                    var b = _buffer[x, y];
+                    if (s != b)
+                    {
+                        _buffer[x, y] = s;
+                        toDraw.Add(new SpriteDisplay(x, y, s));
+                    }
+                }
+            }
+
+            return toDraw;
+        }
+
+        public void DrawOnBackground(int x, int y, SpriteSource sprite)
+        {
+            if (x >= 0 && x < _width && y >= 0 && y < _height)
+                _screen[x, y] = sprite;
+        }
+    }
+}
---
+++ NPacMan.SharedUi/SpriteDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NPacMan.SharedUi
+{
+    public readonly struct SpriteDisplay
+    {
+        public int XPos { get; }
+        public int YPos { get; }
+        public SpriteSource Sprite { get; }
+
+        public SpriteDisplay(int x, int y, SpriteSource sprite)
+        {
+            XPos = x;
+            YPos = y;
+            Sprite = sprite;
+        }
+    }
+}
