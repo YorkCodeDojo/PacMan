3873fd05323c5953a6a62cf6c645cff5f312daf6
ci@yorkdevelopers.org <ci@yorkdevelopers.org>
2020-06-01 21:12:57 +0000

    Updating site

---
+++ JsInteropKeyPress.js
@@ -0,0 +1,17 @@
+﻿document.onkeydown = function (evt) {
+    evt = evt || window.event;
+    window.DotNet.invokeMethodAsync('NPacMan.Blazor', 'JsKeyDown', evt.keyCode);
+
+    //Prevent all but F5 and F12
+    if (evt.keyCode !== 116 && evt.keyCode !== 123)
+        evt.preventDefault();
+};
+
+document.onkeyup = function (evt) {
+    evt = evt || window.event;
+    window.DotNet.invokeMethodAsync('NPacMan.Blazor', 'JsKeyUp', evt.keyCode);
+
+    //Prevent all but F5 and F12
+    if (evt.keyCode !== 116 && evt.keyCode !== 123)
+        evt.preventDefault();
+};
