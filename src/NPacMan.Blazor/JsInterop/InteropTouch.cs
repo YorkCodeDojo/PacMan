c047a5e49b4fed1091d3d8a0c4a994a6beff14b8
Kevin Smith <kev_bite@msn.com>
2020-06-05 22:14:49 +0100

    Types to own files

--- src/NPacMan.Blazor/JsInterop/InteropTouch.cs
+++ src/NPacMan.Blazor/JsInterop/InteropTouch.cs
@@ -1,53 +1,9 @@
 ﻿using System;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Microsoft.JSInterop;
 
 namespace NPacMan.Blazor.JsInterop
 {
-
-    public class TouchEvent
-    {
-        
-        [JsonPropertyName("altKey")]
-        public bool AltKey { get; set; }
-        
-        [JsonPropertyName("changedTouches")]
-        public Touch[] ChangedTouches { get; set; } = new Touch[0];
-        
-        [JsonPropertyName("ctrlKey")]
-        public bool CtrlKey { get; set; }
-
-        [JsonPropertyName("metaKey")]
-        public bool MetaKey { get; set; }
-
-        [JsonPropertyName("shiftKey")]
-        public bool ShiftKey { get; set; }
-        
-        [JsonPropertyName("targetTouches")]
-        public Touch[] TargetTouches { get; set; } = new Touch[0];
-
-        [JsonPropertyName("touches")]
-        public Touch[] Touches { get; set; } = new Touch[0];
-
-        public class Touch
-        {
-            [JsonPropertyName("identifier")]
-            public double Identifier { get; set; }
-            [JsonPropertyName("screenX")]
-            public double ScreenX { get; set; }
-            [JsonPropertyName("screenY")]
-            public double ScreenY { get; set; }
-            [JsonPropertyName("clientX")]
-            public double ClientX { get; set; }
-            [JsonPropertyName("clientY")]
-            public double ClientY { get; set; }
-            [JsonPropertyName("pageX")]
-            public double PageX { get; set; }
-            [JsonPropertyName("pageY")]
-            public double PageY { get; set; }
-        }
-    }
     /// <summary>
     /// Proxy for receiving touch events from JavaScript.
     /// </summary>
--- src/NPacMan.Game/Game.cs
+++ src/NPacMan.Game/Game.cs
@@ -6,17 +6,6 @@ using System.Threading.Tasks;
 
 namespace NPacMan.Game
 {
-    public class Fruit
-    {
-        public CellLocation Location { get; }
-        public FruitType Type { get; }
-
-        public Fruit(CellLocation location, FruitType type)
-        {
-            this.Location = location;
-            this.Type = type;
-        }
-    }
     public class Game
     {
 
