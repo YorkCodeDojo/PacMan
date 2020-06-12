aba9765e32c294b3a6f5ebfff2f698df9f1c8f78
Kevin Smith <kev_bite@msn.com>
2020-06-12 21:27:56 +0100

    Test refactoring

--- test/NPacMan.Game.Tests/GameTests/BonusLivesTests.cs
+++ test/NPacMan.Game.Tests/GameTests/BonusLivesTests.cs
@@ -1,54 +1,86 @@
 using FluentAssertions;
-using FluentAssertions.Execution;
 using Xunit;
-using System.Linq;
 using NPacMan.Game.Tests.Helpers;
 using System.Threading.Tasks;
 using System;
-using static NPacMan.Game.Tests.Helpers.Ensure;
 
 namespace NPacMan.Game.Tests.GameTests
 {
     public class BonusLivesTests
     {
-
         private readonly DateTime _now;
-        private readonly TestGameSettings _gameSettings;
         private readonly TestGameClock _gameClock;
 
         public BonusLivesTests()
         {
             _now = DateTime.UtcNow;
-
-            _gameSettings = new TestGameSettings();
             _gameClock = new TestGameClock();
         }
 
-        [Theory]
-        [InlineData(1, 1)]
-        [InlineData(2, 1)]
-        [InlineData(2, 2)]
-        public async Task LivesIncreaseWhenScoreReachesBonusLifePointAfterEatingCoin(int numberOfCoins, int numberOfTicks)
+        [Fact]
+        public async Task LivesIncreaseWhenScoreReachesBonusLifePointAfterEatingCoin()
         {
-            var pacManStartingLocation = _gameSettings.PacMan.Location;
-            for (int c = 1; c <= numberOfCoins; c++)
-            {
-                _gameSettings.Coins.Add((pacManStartingLocation.X, pacManStartingLocation.Y + c));
-            }
-
-            _gameSettings.PointsNeededForBonusLife = 5;
-
-            var game = new Game(_gameClock, _gameSettings);
+            var game = CreateInitialGameSettings(gameSettings => 
+                {
+                    gameSettings.Coins.Add(gameSettings.PacMan.Location.Below);
+                    gameSettings.Coins.Add(gameSettings.PacMan.Location.FarAway());
+                });
             game.StartGame();
 
             await game.ChangeDirection(Direction.Down);
 
             var previousLives = game.Lives;
 
-            for(int t = 0; t < numberOfTicks; t++)
-            {
-                await _gameClock.Tick();
-            }
+            await _gameClock.Tick();
+
+            game.Lives.Should().Be(previousLives + 1);
+        }
+
+        private Game CreateInitialGameSettings(Action<TestGameSettings> configure)
+        {
+            var gameSettings = new TestGameSettings();            
+
+            gameSettings.PointsNeededForBonusLife = 5;
+
+            configure(gameSettings);
+
+            return new Game(_gameClock, gameSettings);
+        }
+
+        [Fact]
+        public async Task LivesIncreaseWhenScoreReachesBonusLifePointAfterEatingCoinThatCompletesLevel()
+        {
+            var game = CreateInitialGameSettings(gameSettings =>
+                        gameSettings.Coins.Add(gameSettings.PacMan.Location.Below));
+                        
+            var previousLives = game.Lives;
+
+            await PlayUntilBonusLife(game);
+
+            game.Lives.Should().Be(previousLives + 1);
+        }
+
+        private async Task PlayUntilBonusLife(Game game)
+        {
+             game.StartGame();
+
+            await game.ChangeDirection(Direction.Down);
+
+            await _gameClock.Tick();
+        }
+
+        [Fact]
+        public async Task LivesIncreaseOnceWhenScoreReachesBonusLifePointAfterScoreIncreasesFurther()
+        {
+            var game = CreateInitialGameSettings(gameSettings => {
+                gameSettings.Coins.Add(gameSettings.PacMan.Location.Below);
+                gameSettings.Coins.Add(gameSettings.PacMan.Location.Below.Below);
+                gameSettings.Coins.Add(gameSettings.PacMan.Location.FarAway());
+            });
+            var previousLives = game.Lives;
+            await PlayUntilBonusLife(game);
+
+            await _gameClock.Tick();
 
             game.Lives.Should().Be(previousLives + 1);
         }
