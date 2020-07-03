bf557eec70163112c250c2159305bb90b5457939
Kevin Smith <kev_bite@msn.com>
2020-07-03 19:15:14 +0100

    Refactoring MoveClockTests #96

--- test/NPacMan.Game.Tests/MoveClockTests.cs
+++ test/NPacMan.Game.Tests/MoveClockTests.cs
@@ -7,120 +7,75 @@ namespace NPacMan.Game.Tests
 {
     public class MoveClockTests
     {
+        private static class PercentagesInMilliseconds
+        {
+            // Percent => Milliseconds : m = 100 / (p / 100)
+            public const int Percent50 = 200;
+            public const int Percent55 = 181;
+            public const int Percent60 = 166;
+            public const int Percent75 = 133;
+            public const int Percent80 = 125;
+            public const int Percent85 = 117;
+            public const int Percent90 = 111;
+            public const int Percent95 = 105;
+            public const int Percent100 = 100;
+        }
+
+
         [Theory]
-        [InlineData(1, 125)]  // 80%
-        [InlineData(2, 111)]  // 90%
-        [InlineData(5, 100)]  // 100%
-        [InlineData(21, 111)]  // 90%
+        [InlineData(1, PercentagesInMilliseconds.Percent80)]  // 80%
+        [InlineData(2, PercentagesInMilliseconds.Percent90)]  // 90%
+        [InlineData(5, PercentagesInMilliseconds.Percent100)]  // 100%
+        [InlineData(21, PercentagesInMilliseconds.Percent90)]  // 90%
         public void PacManShouldTravelAt80PercentAtLevelZero(int level, int moveSpeedMilliseconds)
         {
-            var moveClock = new MoveClock();
-            
-            moveClock.UpdateTime(TimeSpan.FromMinutes(1));
-            if(!moveClock.ShouldPacManMove(level, false))
-            {
-                throw new Exception("PacMan Should always move on first move");
-            }
-
-            for (var j =0; j < 3; j++)
-            {
-                for (var i = 0; i < moveSpeedMilliseconds-1; i++)
-                {
-                    moveClock.UpdateTime(TimeSpan.FromMilliseconds(1));
-                    if(moveClock.ShouldPacManMove(level, false))
-                    {
-                        throw new Exception($"PacMan moved early after just {i} milliseconds");
-                    }
-                }
-
-                moveClock.UpdateTime(TimeSpan.FromMilliseconds(1));
-
-                moveClock.ShouldPacManMove(level, false).Should().BeTrue();
-                moveClock.ShouldPacManMove(level, false).Should().BeFalse();
-            }
+            Assert(moveSpeedMilliseconds,
+                   moveClock => moveClock.ShouldPacManMove(level, false));
         }
 
         [Theory]
-        [InlineData(1, 111)]  // 90%
-        [InlineData(2, 105)]  // 95%
-        [InlineData(5, 100)]  // 100%
-        [InlineData(21, 105)]  // 95%
+        [InlineData(1, PercentagesInMilliseconds.Percent90)]  // 90%
+        [InlineData(2, PercentagesInMilliseconds.Percent95)]  // 95%
+        [InlineData(5, PercentagesInMilliseconds.Percent100)]  // 100%
+        [InlineData(21, PercentagesInMilliseconds.Percent95)]  // 95%
         public void PacManShouldTravelAtDifferentSpeedsForDifferentLevelsWhenFrightened(int level, int moveSpeedMilliseconds)
         {
-            var moveClock = new MoveClock();
-            
-            moveClock.UpdateTime(TimeSpan.FromMinutes(1));
-            if(!moveClock.ShouldPacManMove(level, true))
-            {
-                throw new Exception("PacMan Should always move on first move");
-            }
-
-            for (var j =0; j < 3; j++)
-            {
-                for (var i = 0; i < moveSpeedMilliseconds-1; i++)
-                {
-                    moveClock.UpdateTime(TimeSpan.FromMilliseconds(1));
-                    if(moveClock.ShouldPacManMove(level, isFrightened: true))
-                    {
-                        throw new Exception($"PacMan moved early after just {i} milliseconds");
-                    }
-                }
-
-                moveClock.UpdateTime(TimeSpan.FromMilliseconds(1));
-
-                moveClock.ShouldPacManMove(level, true).Should().BeTrue();
-                moveClock.ShouldPacManMove(level, true).Should().BeFalse();
-            }
+            Assert(moveSpeedMilliseconds,
+                   moveClock => moveClock.ShouldPacManMove(level, true) );     
         }
 
-        
         [Theory]
-        [InlineData(1, 200, GhostStatus.Edible)]  //  50%
-        [InlineData(1, 200, GhostStatus.Flash)]  //  50%
-        [InlineData(4, 181, GhostStatus.Edible)]  //  55%
-        [InlineData(4, 181, GhostStatus.Flash)]  //  55%
-        [InlineData(5, 166, GhostStatus.Edible)]  //  60%
-        [InlineData(5, 166, GhostStatus.Flash)]  //  60%
+        [InlineData(1, PercentagesInMilliseconds.Percent50, GhostStatus.Edible)]  //  50%
+        [InlineData(1, PercentagesInMilliseconds.Percent50, GhostStatus.Flash)]  //  50%
+        [InlineData(4, PercentagesInMilliseconds.Percent55, GhostStatus.Edible)]  //  55%
+        [InlineData(4, PercentagesInMilliseconds.Percent55, GhostStatus.Flash)]  //  55%
+        [InlineData(5, PercentagesInMilliseconds.Percent60, GhostStatus.Edible)]  //  60%
+        [InlineData(5, PercentagesInMilliseconds.Percent60, GhostStatus.Flash)]  //  60%
         public void GhostShouldTravelAtDifferentSpeedsForDifferentLevelsWhenFrightened(int level, int moveSpeedMilliseconds, GhostStatus ghostStatus)
         {
-            var moveClock = new MoveClock();
-            var ghostName = "ghost1";
-            moveClock.UpdateTime(TimeSpan.FromMinutes(1));
-            if(!moveClock.ShouldGhostMove(level, ghostName, ghostStatus))
-            {
-                throw new Exception("Ghost Should always move on first move");
-            }
-
-            for (var j =0; j < 3; j++)
-            {
-                for (var i = 0; i < moveSpeedMilliseconds-1; i++)
-                {
-                    moveClock.UpdateTime(TimeSpan.FromMilliseconds(1));
-                    if(moveClock.ShouldGhostMove(level, ghostName, ghostStatus))
-                    {
-                        throw new Exception($"Ghost moved early after just {i} milliseconds");
-                    }
-                }
-
-                moveClock.UpdateTime(TimeSpan.FromMilliseconds(1));
-
-                moveClock.ShouldGhostMove(level, ghostName, ghostStatus).Should().BeTrue();
-                moveClock.ShouldGhostMove(level, ghostName, ghostStatus).Should().BeFalse();
-            }
+            var ghostName = Guid.NewGuid().ToString();
+            Assert(moveSpeedMilliseconds,
+                   moveClock => moveClock.ShouldGhostMove(level, ghostName, ghostStatus) );            
         }
 
         [Theory]
-        [InlineData(1, 133)]  //  75%
-        [InlineData(4, 117)]  //  85%
-        [InlineData(5, 105)]  //  95%
+        [InlineData(1, PercentagesInMilliseconds.Percent75)]  //  75%
+        [InlineData(4, PercentagesInMilliseconds.Percent85)]  //  85%
+        [InlineData(5, PercentagesInMilliseconds.Percent95)]  //  95%
         public void GhostShouldTravelAtDifferentSpeedsForDifferentLevelsWhenAlive(int level, int moveSpeedMilliseconds)
         {
+            var ghostName = Guid.NewGuid().ToString();
+            Assert(moveSpeedMilliseconds,
+                   moveClock => moveClock.ShouldGhostMove(level, ghostName, GhostStatus.Alive) );
+        }
+
+        private void Assert(int moveSpeedMilliseconds, Func<MoveClock, bool> action)
+        {
             var moveClock = new MoveClock();
-            var ghostName = "ghost1";
             moveClock.UpdateTime(TimeSpan.FromMinutes(1));
-            if(!moveClock.ShouldGhostMove(level, ghostName, GhostStatus.Alive))
+            if(!action(moveClock))
             {
-                throw new Exception("Ghost Should always move on first move");
+                throw new Exception("Game piece should always move on first move");
             }
 
             for (var j =0; j < 3; j++)
@@ -128,17 +83,18 @@ namespace NPacMan.Game.Tests
                 for (var i = 0; i < moveSpeedMilliseconds-1; i++)
                 {
                     moveClock.UpdateTime(TimeSpan.FromMilliseconds(1));
-                    if(moveClock.ShouldGhostMove(level, ghostName, GhostStatus.Alive))
+                    if(action(moveClock))
                     {
-                        throw new Exception($"Ghost moved early after just {i} milliseconds");
+                        throw new Exception($"Game piece moved early after just {i} milliseconds");
                     }
                 }
 
                 moveClock.UpdateTime(TimeSpan.FromMilliseconds(1));
 
-                moveClock.ShouldGhostMove(level, ghostName, GhostStatus.Alive).Should().BeTrue();
-                moveClock.ShouldGhostMove(level, ghostName, GhostStatus.Alive).Should().BeFalse();
+                action(moveClock).Should().BeTrue();
+                action(moveClock).Should().BeFalse();
             }
         }
+
     }
 }
