7aa2bd713bcaa0eb8cd11a673fa654aa29ef1286
Kevin Smith <kev_bite@msn.com>
2020-07-03 19:56:15 +0100

    Refactoring MoveClockTests #96

--- test/NPacMan.Game.Tests/MoveClockTests.cs
+++ test/NPacMan.Game.Tests/MoveClockTests.cs
@@ -1,35 +1,61 @@
 using FluentAssertions;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using Xunit;
 
 namespace NPacMan.Game.Tests
 {
-    public class MoveClockTests
+    public class PacMPacManShouldTravelAtDifferentSpeedsForDifferentLevelsWhenNotFrightenedTestData : IEnumerable<object[]>
     {
-        private static class PercentagesInMilliseconds
+        private object[] CreateTestData(int level, int milliseconds)
         {
-            // Percent => Milliseconds : m = 100 / (p / 100)
-            public const int Percent50 = 200;
-            public const int Percent55 = 181;
-            public const int Percent60 = 166;
-            public const int Percent75 = 133;
-            public const int Percent80 = 125;
-            public const int Percent85 = 117;
-            public const int Percent90 = 111;
-            public const int Percent95 = 105;
-            public const int Percent100 = 100;
-            public const int Percent105 = 95;
-            public const int Percent160 = 62;
+            return new object[]{level, milliseconds};
+        }
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return CreateTestData(1, PercentagesInMilliseconds.Percent80);
+
+            for (var level = 2; level <= 4; level++)
+            {
+                yield return CreateTestData(level, PercentagesInMilliseconds.Percent90);
+            }
+
+            for (var level = 5; level <= 20; level++)
+            {
+                yield return CreateTestData(level, PercentagesInMilliseconds.Percent100);
+            }
+            
+            for (var level = 21; level <= 256; level++)
+            {
+                yield return CreateTestData(level, PercentagesInMilliseconds.Percent90);
+            }
         }
 
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
 
+    public static class PercentagesInMilliseconds
+    {
+        // Percent => Milliseconds : m = 100 / (p / 100)
+        public const int Percent50 = 200;
+        public const int Percent55 = 181;
+        public const int Percent60 = 166;
+        public const int Percent75 = 133;
+        public const int Percent80 = 125;
+        public const int Percent85 = 117;
+        public const int Percent90 = 111;
+        public const int Percent95 = 105;
+        public const int Percent100 = 100;
+        public const int Percent105 = 95;
+        public const int Percent160 = 62;
+    }
+
+    public class MoveClockTests
+    {
         [Theory]
-        [InlineData(1, PercentagesInMilliseconds.Percent80)]  // 80%
-        [InlineData(2, PercentagesInMilliseconds.Percent90)]  // 90%
-        [InlineData(5, PercentagesInMilliseconds.Percent100)]  // 100%
-        [InlineData(21, PercentagesInMilliseconds.Percent90)]  // 90%
-        public void PacManShouldTravelAt80PercentAtLevelZero(int level, int moveSpeedMilliseconds)
+        [ClassData(typeof(PacMPacManShouldTravelAtDifferentSpeedsForDifferentLevelsWhenNotFrightenedTestData))]
+        public void PacManShouldTravelAtDifferentSpeedsForDifferentLevelsWhenNotFrightened(int level, int moveSpeedMilliseconds)
         {
             TestMoveClock(moveSpeedMilliseconds,
                    moveClock => moveClock.ShouldPacManMove(level, false));
@@ -43,7 +69,7 @@ namespace NPacMan.Game.Tests
         public void PacManShouldTravelAtDifferentSpeedsForDifferentLevelsWhenFrightened(int level, int moveSpeedMilliseconds)
         {
             TestMoveClock(moveSpeedMilliseconds,
-                   moveClock => moveClock.ShouldPacManMove(level, true) );     
+                   moveClock => moveClock.ShouldPacManMove(level, true));
         }
 
         [Theory]
@@ -58,7 +84,7 @@ namespace NPacMan.Game.Tests
             var ghostName = Guid.NewGuid().ToString();
 
             TestMoveClock(moveSpeedMilliseconds,
-                   moveClock => moveClock.ShouldGhostMove(level, coinsRemaining: int.MaxValue, ghostName, ghostStatus) );            
+                   moveClock => moveClock.ShouldGhostMove(level, coinsRemaining: int.MaxValue, ghostName, ghostStatus));
         }
 
         [Theory]
@@ -70,7 +96,7 @@ namespace NPacMan.Game.Tests
             var ghostName = Guid.NewGuid().ToString();
 
             TestMoveClock(moveSpeedMilliseconds,
-                   moveClock => moveClock.ShouldGhostMove(level, coinsRemaining: int.MaxValue, ghostName, GhostStatus.Alive) );
+                   moveClock => moveClock.ShouldGhostMove(level, coinsRemaining: int.MaxValue, ghostName, GhostStatus.Alive));
         }
 
         [Theory]
@@ -80,9 +106,9 @@ namespace NPacMan.Game.Tests
         public void GhostShouldTravelAtDifferentSpeedsForDifferentLevelsWhenRunningHome(int level, int moveSpeedMilliseconds)
         {
             var ghostName = Guid.NewGuid().ToString();
-            
+
             TestMoveClock(moveSpeedMilliseconds,
-                   moveClock => moveClock.ShouldGhostMove(level, coinsRemaining: int.MaxValue, ghostName, GhostStatus.RunningHome) );
+                   moveClock => moveClock.ShouldGhostMove(level, coinsRemaining: int.MaxValue, ghostName, GhostStatus.RunningHome));
         }
 
         [Theory]
@@ -111,24 +137,24 @@ namespace NPacMan.Game.Tests
         {
             var ghostName = GhostNames.Blinky;
             TestMoveClock(moveSpeedMilliseconds,
-                   moveClock => moveClock.ShouldGhostMove(level, coinsLeft, ghostName, GhostStatus.Alive) );
+                   moveClock => moveClock.ShouldGhostMove(level, coinsLeft, ghostName, GhostStatus.Alive));
         }
 
         private void TestMoveClock(int moveSpeedMilliseconds, Func<MoveClock, bool> action)
         {
             var moveClock = new MoveClock();
             moveClock.UpdateTime(TimeSpan.FromMinutes(1));
-            if(!action(moveClock))
+            if (!action(moveClock))
             {
                 throw new Exception("Game piece should always move on first move");
             }
 
-            for (var j =0; j < 3; j++)
+            for (var j = 0; j < 3; j++)
             {
-                for (var i = 0; i < moveSpeedMilliseconds-1; i++)
+                for (var i = 0; i < moveSpeedMilliseconds - 1; i++)
                 {
                     moveClock.UpdateTime(TimeSpan.FromMilliseconds(1));
-                    if(action(moveClock))
+                    if (action(moveClock))
                     {
                         throw new Exception($"Game piece moved early after just {i} milliseconds");
                     }
